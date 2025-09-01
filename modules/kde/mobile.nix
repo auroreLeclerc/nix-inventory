46f7669e0cf84b5ae14d10f1c8c85ecbee45dbb7
auroreLeclerc <aurore.leclerc.lefranc@outlook.fr>
2025-09-01 21:29:37 +0200

    WIP

--- modules/kde/mobile.nix
+++ modules/kde/mobile.nix
@@ -6,7 +6,7 @@
 		defaultSession = lib.mkForce "plasma-mobile";
 	};
 	environment.systemPackages = (with pkgs.kdePackages; [
-		plasma-mobile plasma-nano keysmith koko plasma-dialer spacebar
-	]) ++ (with pkgs; [ maliit-framework maliit-keyboardcalindori ]);
+		plasma-mobile plasma-nano keysmith koko plasma-dialer spacebar calindori
+	]) ++ (with pkgs; [ maliit-framework maliit-keyboard ]);
 	hardware.sensor.iio.enable = true;
 }
