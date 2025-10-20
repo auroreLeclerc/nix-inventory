6cb0f694362d7c24cd8e03651a6ea4dc81b94995
auroreLeclerc <aurore.leclerc.lefranc@outlook.fr>
2025-10-20 18:21:23 +0200

    misc

--- modules/core/user.nix
+++ modules/core/user.nix
@@ -7,7 +7,7 @@
 				efi.canTouchEfiVariables = true;
 			};
 			plymouth = {
-				enable = lib.mkIf config.desktopManager.plasma6.enable;
+				enable = config.desktopManager.plasma6.enable;
 				theme = "angular";
 				themePackages = with pkgs; [
 					(adi1090x-plymouth-themes.override {
