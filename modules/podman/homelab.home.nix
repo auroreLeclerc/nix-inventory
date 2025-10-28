17522b2f55517597f9f0b7325ff0986c15c37a38
auroreLeclerc <aurore.leclerc.lefranc@outlook.fr>
2025-10-28 11:30:18 +0100

    WIP

--- modules/podman/homelab.home.nix
+++ modules/podman/homelab.home.nix
@@ -31,7 +31,8 @@
 					certificatesresolvers.duckresolver.acme = {
 						dnschallenge = {
 							provider = "duckdns";
-							propagation.disableChecks = true;
+							# propagation.disableChecks = true;
+							propagation.delaybeforechecks = 120;
 							# propagation.disableANSChecks = true;
 							# propagation.requireAllRNS = false;
 						};
