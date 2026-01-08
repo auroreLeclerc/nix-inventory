f968bdeb7349dfcbf4007b09b082ae3df3914120
auroreLeclerc <aurore.leclerc.lefranc@outlook.fr>
2026-01-09 00:46:28 +0100

    import

--- units/fierce-deity/configuration.nix
+++ units/fierce-deity/configuration.nix
@@ -8,7 +8,6 @@
 		../../modules/kde/kde.nix
 		../../modules/gaming/gaming.nix
 		../../modules/workstation.nix
-		../../modules/podman/workbench.home.nix
 		inputs.nixos-hardware.nixosModules.common-cpu-amd
 		inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
 		inputs.nixos-hardware.nixosModules.common-cpu-amd-zenpower
@@ -16,6 +15,7 @@
 		{
 			home-manager.users.dawn = {
 				imports = [
+					../../modules/podman/workbench.home.nix
 					../../modules/firefox.home.nix
 					../../modules/kde/kde.home.nix
 					../../modules/flatpak.home.nix
