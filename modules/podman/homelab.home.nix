f5f7d46d8c047b7e211692e7c407efc868fa1c2a
auroreLeclerc <aurore.leclerc.lefranc@outlook.fr>
2026-05-16 19:21:56 +0200

    test

--- modules/podman/homelab.home.nix
+++ modules/podman/homelab.home.nix
@@ -103,8 +103,8 @@ in
             extraPodmanArgs = [
               "--sysctl net.ipv4.conf.all.src_valid_mark=1"
               "--sysctl net.ipv4.ip_forward=1"
-              "--network=docker-like:ip=172.18.0.2"
-              "--network=friends:ip=172.19.0.2"
+              # "--network=docker-like:ip=172.18.0.2"
+              # "--network=friends:ip=172.19.0.2"
             ];
             autoUpdate = "registry";
           };
