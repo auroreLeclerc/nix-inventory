4bd1ca5b7a30d1ce54ab4c2775670851d5ac0fd3
auroreLeclerc <aurore.leclerc.lefranc@outlook.fr>
2026-06-22 11:13:41 +0200

    fix

--- modules/vscode.home.nix
+++ modules/vscode.home.nix
@@ -88,44 +88,44 @@
       pre-commit
       android-tools
     ];
-  };
-  file = {
-    css = {
-      source = builtins.fetchurl {
-        url = "https://www.dl.dropboxusercontent.com/scl/fi/mk0zd3iavho0asyh7zm2y/CSS-selectors-cheatsheet.pdf?rlkey=2wn3w9bkbizcjpi5f822brj6m&e=2&st=q0d5f1uh&dl=1";
-        name = "CSS-selectors-cheatsheet.pdf";
-        sha256 = "sha256-hZXNDiT+nk6KbdKctCPeZqv7MBx2C8b7224l3rAXhew=";
+    file = {
+      css = {
+        source = builtins.fetchurl {
+          url = "https://www.dl.dropboxusercontent.com/scl/fi/mk0zd3iavho0asyh7zm2y/CSS-selectors-cheatsheet.pdf?rlkey=2wn3w9bkbizcjpi5f822brj6m&e=2&st=q0d5f1uh&dl=1";
+          name = "CSS-selectors-cheatsheet.pdf";
+          sha256 = "sha256-hZXNDiT+nk6KbdKctCPeZqv7MBx2C8b7224l3rAXhew=";
+        };
+        target = "Bureau/CSS selectors cheatsheet.pdf";
       };
-      target = "Bureau/CSS selectors cheatsheet.pdf";
-    };
-    html = {
-      source = builtins.fetchurl {
-        url = "https://user.oc-static.com/upload/2022/11/25/16693925991605_FR_1603881_HTML-CSS_Static-Graphics_p3c1-1.jpg";
-        sha256 = "sha256-om0kUy1I8gOb1NK+7FqI8JnnGvzv3mhiLQBZJoFtcRY=";
+      html = {
+        source = builtins.fetchurl {
+          url = "https://user.oc-static.com/upload/2022/11/25/16693925991605_FR_1603881_HTML-CSS_Static-Graphics_p3c1-1.jpg";
+          sha256 = "sha256-om0kUy1I8gOb1NK+7FqI8JnnGvzv3mhiLQBZJoFtcRY=";
+        };
+        target = "Bureau/Structurez votre page.jpg";
       };
-      target = "Bureau/Structurez votre page.jpg";
-    };
-    strategique = {
-      source = builtins.fetchurl {
-        url = "https://messervices.cyber.gouv.fr/documents-guides/20231218_Volet_strat%C3%A9gique_cyberattaquesetrem%C3%A9diation_v1g.pdf";
-        name = "volet-strategique.pdf";
-        sha256 = "sha256-ASn4GzwXZj5gsLBcz4RyK8gsofksha+k0eVtwm1d0KA=";
+      strategique = {
+        source = builtins.fetchurl {
+          url = "https://messervices.cyber.gouv.fr/documents-guides/20231218_Volet_strat%C3%A9gique_cyberattaquesetrem%C3%A9diation_v1g.pdf";
+          name = "volet-strategique.pdf";
+          sha256 = "sha256-ASn4GzwXZj5gsLBcz4RyK8gsofksha+k0eVtwm1d0KA=";
+        };
+        target = "Bureau/Volet stratégique.pdf";
       };
-      target = "Bureau/Volet stratégique.pdf";
-    };
-    operationnel = {
-      source = builtins.fetchurl {
-        url = "https://messervices.cyber.gouv.fr/documents-guides/20231218_Volet_operationnel_cyberattaquesetremediation_a5_v1j.pdf";
-        sha256 = "sha256-MAcd0Nx3CD3YTphLobfB84R6qpoCNWb0SdrziGVpPys=";
+      operationnel = {
+        source = builtins.fetchurl {
+          url = "https://messervices.cyber.gouv.fr/documents-guides/20231218_Volet_operationnel_cyberattaquesetremediation_a5_v1j.pdf";
+          sha256 = "sha256-MAcd0Nx3CD3YTphLobfB84R6qpoCNWb0SdrziGVpPys=";
+        };
+        target = "Bureau/Volet opérationnel.pdf";
       };
-      target = "Bureau/Volet opérationnel.pdf";
-    };
-    technique = {
-      source = builtins.fetchurl {
-        url = "https://messervices.cyber.gouv.fr/documents-guides/20231218_Volet_technique_cyberattaquesetremediation_a5_v1h.pdf";
-        sha256 = "sha256-MrAcv3PqlmW1uyigRV80GwQGcTyQkRFtDOuvBN82v8c=";
+      technique = {
+        source = builtins.fetchurl {
+          url = "https://messervices.cyber.gouv.fr/documents-guides/20231218_Volet_technique_cyberattaquesetremediation_a5_v1h.pdf";
+          sha256 = "sha256-MrAcv3PqlmW1uyigRV80GwQGcTyQkRFtDOuvBN82v8c=";
+        };
+        target = "Bureau/Volet technique.pdf";
       };
-      target = "Bureau/Volet technique.pdf";
     };
   };
 }
